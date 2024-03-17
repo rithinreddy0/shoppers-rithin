39d6da70a6045d148199648ebe40979442711616
rithinreddy0 <135361821+rithinreddy0@users.noreply.github.com>
2024-03-17 18:47:08 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -23,7 +23,7 @@ export default function Admin() {
     const senddata = async ()=>{
         const response = await fetch(url, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "no-cors", // no-cors, *cors, same-origin
+            mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, *same-origin, omit
             headers: {
