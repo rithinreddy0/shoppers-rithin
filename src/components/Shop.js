4bd0a2e74f67f442461b6500c884fd580d90edcf
rithinreddy0 <135361821+rithinreddy0@users.noreply.github.com>
2024-03-17 18:13:30 +0530

    Update Shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -10,7 +10,9 @@ export default function Shop() {
     })
     const url = "https://backend-4-zfpb.onrender.com/getallproducts"
     async function receivedata(){
-      const response = await fetch(url);
+      const response = await fetch(url,{
+        mode:"no-cors"
+                                  );
       const data = await response.json();
       setproducts(data.data);
      
